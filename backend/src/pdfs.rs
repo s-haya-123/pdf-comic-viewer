c8c2ff41aa991cfac9f97585aa95785d70fc81ff
s-haya-123 <ganbaru.wakenai@gmail.com>
2020-03-30 01:03:25 +0900

    feat: return json sample

---
+++ backend/src/pdfs.rs
@@ -0,0 +1,24 @@
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PDF {
+    pub id: String,
+    pub author: String,
+    pub title: String,
+    pub thumbnail: String,
+    pub pdf: String,
+    // pub tag: [String]
+}
+
+#[get("/pdf")]
+pub fn pdf() -> Json<Vec<PDF>> {
+    Json(vec![PDF {
+        id: "1".into(),
+        author: "author".into(),
+        title: "titile".into(),
+        thumbnail: "http://thumb".into(),
+        pdf: "http://pdf".into(),
+        // tag: ["tag".into()]
+    }])
+}
