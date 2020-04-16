47dbff5f117b15af82225f342ee3071367b02b0f
s-haya-123 <ganbaru.wakenai@gmail.com>
2020-04-16 21:49:24 +0900

    feat: read from postgres

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+
+#[macro_use] extern crate diesel;
 mod pdfs;
+mod models;
 
 use pdfs::*;
+use backend::*;
+use models::*;
+use diesel::prelude::*;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -11,5 +17,15 @@ fn index() -> &'static str {
 }
 
 fn main() {
+    use schema::comic::dsl::*;
+    let connection = establish_connection();
+    let results = comic
+    .load::<Comic>(&connection)
+    .expect("Error loading posts");
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+    }
     rocket::ignite().mount("/", routes![index, pdf]).launch();
 }
---
+++ backend/src/models.rs
@@ -0,0 +1,11 @@
+
+#[derive(Queryable)]
+pub struct Comic {
+    pub id: i32,
+    pub author: Option<String>,
+    pub title: String,
+    pub thumbnail: Option<String>,
+    pub pdf: Option<String>,
+    pub tab: Option<Vec<String>>,
+    pub folder_id: Option<String>
+}
