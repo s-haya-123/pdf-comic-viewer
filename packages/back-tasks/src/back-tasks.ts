84c21f016cf40d98b8a34a47b1f6afbad96b5f72
s-haya-123 <ganbaru.wakenai@gmail.com>
2020-04-25 20:28:56 +0900

    feat: write pdf thumbnail

--- packages/back-tasks/src/back-tasks.ts
+++ packages/back-tasks/src/back-tasks.ts
@@ -1,2 +1,41 @@
-export function backTasks() {
+import { getPDFFactory, getPDFViewport } from './readPdfNode';
+import { createCanvas } from 'canvas';
+import { writeFile } from 'fs';
+import { PDFRenderParams, PDFDocumentProxy } from 'pdfjs-dist';
+const getDocument = require('pdfjs-dist/es5/build/pdf').getDocument;
+
+class CanvasFactory {
+    create(width: number, height: number) {
+        var canvas = createCanvas(width, height);
+        var context = canvas.getContext("2d");
+        return {
+            canvas: canvas,
+            context: context,
+        };
+    }
+    reset(ctx: CanvasRenderingContext2D, width: number, height: number) {
+        ctx.canvas.width = width;
+        ctx.canvas.height = height;
+    }
+    destroy(ctx: CanvasRenderingContext2D) {
+        ctx.canvas.width = 0;
+        ctx.canvas.height = 0;
+    }
+}
+export async function backTasks(pdfPath: string) {
+    const factory = await getPDFFactory(pdfPath);
+    const {width, height} = await factory.getPDFSize();
+    const canvas = createCanvas(width, height);
+    const page = await factory.getPage(1);
+    const ctx = canvas.getContext('2d')!;
+    const config = getPDFViewport(page, ctx);
+    const canvasFactory = new CanvasFactory();
+    await ((page.render({...config, canvasFactory} as PDFRenderParams).promise as unknown) as Promise<void>);
+    writeFile('./data.jpg', canvas.toBuffer(), ()=>{});
+}
+
+if (process.argv.length >= 2) {
+    backTasks(process.argv[2]);
+} else {
+    console.log('required pdf files path');
 }
---
+++ packages/back-tasks/src/readPdfNode.ts
@@ -0,0 +1,32 @@
+import { PDFDocumentProxy, PDFPageProxy } from 'pdfjs-dist';
+const getDocument = require('pdfjs-dist/es5/build/pdf').getDocument;
+export interface PDFFactory {
+    getPDFSize: () => Promise<{width: number, height: number, pageNumber: number}>;
+    getPage: (page: number) => Promise<PDFPageProxy>;
+}
+export async function getPDFFactory(src: string | Uint8Array): Promise<PDFFactory> {
+    const docPromise = (getDocument(src).promise as unknown) as Promise<
+    PDFDocumentProxy
+    >;
+    return {
+        getPDFSize: () =>
+            docPromise.then(
+                async doc => {
+                    const page = await ((doc.getPage(1) as unknown) as Promise<PDFPageProxy>);
+                    const { width, height } = page.getViewport({scale: 1});
+                    const pageNumber = doc.numPages;
+                    return { width, height, pageNumber }
+                }
+            ),
+        getPage: (page: number) => docPromise.then(
+            doc => (doc.getPage(page) as unknown) as Promise<PDFPageProxy>
+        )
+    }
+}
+export function getPDFViewport(pdfPage: PDFPageProxy, ctx: CanvasRenderingContext2D, scale = 1) {
+    const viewport = pdfPage.getViewport({ scale });
+    return {
+        canvasContext: ctx,
+        viewport
+    };
+}
