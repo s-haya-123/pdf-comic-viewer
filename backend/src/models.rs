771a00dc9250a8075dfa51aae80d684c3cca9233
s-haya-123 <ganbaru.wakenai@gmail.com>
2020-04-26 22:58:44 +0900

    fix: set uuid in models

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,11 +1,10 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 #[derive(Debug, Serialize, Deserialize, Queryable)]
 pub struct Comic {
-    pub id: i32,
+    pub id: Uuid,
     pub author: Option<String>,
     pub title: String,
-    pub thumbnail: Option<String>,
-    pub pdf: Option<String>,
     pub tag: Option<Vec<String>>,
     pub current_page: Option<i32>,
     pub folder_id: Option<String>
